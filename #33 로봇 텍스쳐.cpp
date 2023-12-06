1b021dcc947a028469241febc31f1aeadec3e9dd
chalbangE <yubin41029@naver.com>
2023-12-06 15:58:09 +0900

    실습 33 완성

---
+++ #33 로봇 텍스쳐.cpp
@@ -0,0 +1,593 @@
+#include "stdafx.h"
+#include "GLShapes.h"
+#include "GLObj.h"
+#include "GLPyramid.h"
+#include "GLLight.h"
+#include "GLLIne.h"
+#include "GLCamera.h"
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+GLuint vertexShader, fragmentShader; //--- 세이더 객체
+GLuint shaderProgramID; //--- 셰이더 프로그램
+
+void make_shaderProgram();
+void make_vertexShaders();
+void make_fragmentShaders();
+void InitBuffer();
+GLvoid Mouse(int button, int state, int x, int y);
+GLvoid Motion(int x, int y);
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+char* filetobuf(const char* file);
+
+void Init();
+void TimerFunction(int value);
+void Keyboard(unsigned char key, int x, int y);
+void Special_Keyboard(int key, int x, int y);
+void MouseWheel(int wheel, int diretion, int x, int y);
+
+using namespace std;
+
+float winSizex = 800, winSizey = 800;
+GLuint vao;
+
+GLObj Object[2];
+GLLine lineObj;
+GLCamera Camera;
+GLLight Light;
+GLObj Floor;
+vector <GLObj> Cube;
+
+int mod = 0;
+bool Lbt = false, Snow_mod = false;
+glm::vec3 click_mouse{};
+
+static std::uniform_real_distribution<GLfloat> Size(0.0f, 0.01f);
+static std::uniform_real_distribution<GLfloat> Pos(-1.f, 1.f);
+
+
+int main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(300, 100);
+	glutInitWindowSize(winSizex, winSizey);
+	glutCreateWindow("#30 텍스쳐");
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+	make_shaderProgram();
+	InitBuffer();
+	Init();
+	glutMouseFunc(Mouse);
+	glutMotionFunc(Motion);
+	glutMouseWheelFunc(MouseWheel);
+	glutKeyboardFunc(Keyboard); // 키보드 입력 콜백함수
+	glutSpecialFunc(Special_Keyboard); // 키보드 입력 콜백함수
+	glutTimerFunc(10, TimerFunction, 1);
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+	glUseProgram(shaderProgramID);
+
+	// 버텍스 쉐이더에게 전달
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position"); //	: 0
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color"); //	: 1
+	int NormalLocation = glGetAttribLocation(shaderProgramID, "in_Normal");
+	int UvLocation = glGetAttribLocation(shaderProgramID, "in_Uv");
+	unsigned int WorldTransLocation = glGetUniformLocation(shaderProgramID, "World_trans");
+	unsigned int CameraLocation = glGetUniformLocation(shaderProgramID, "Camera_trans");
+	unsigned int ProjectionLocation = glGetUniformLocation(shaderProgramID, "Projection_trans");
+	glEnableVertexAttribArray(PosLocation);
+	glEnableVertexAttribArray(ColorLocation);
+	glEnableVertexAttribArray(NormalLocation);
+	glEnableVertexAttribArray(UvLocation);
+	glEnableVertexAttribArray(WorldTransLocation);
+
+
+	// 프래그먼트 쉐이더에게 전달
+	int LightPosLocation = glGetUniformLocation(shaderProgramID, "Light_Pos");
+	unsigned int LightColorLocation = glGetUniformLocation(shaderProgramID, "Light_Color");
+	unsigned int ViewPosLocation = glGetUniformLocation(shaderProgramID, "View_Pos");
+	unsigned int DistanceLocation = glGetUniformLocation(shaderProgramID, "Distance");
+	unsigned int TexSamplerLocation = glGetUniformLocation(shaderProgramID, "out_Tex");
+	unsigned int TexorColorLocation = glGetUniformLocation(shaderProgramID, "Tex_or_Color");
+	glEnableVertexAttribArray(LightPosLocation);
+	glEnableVertexAttribArray(LightColorLocation);
+	glEnableVertexAttribArray(ViewPosLocation);
+	glEnableVertexAttribArray(DistanceLocation);
+	glEnableVertexAttribArray(TexorColorLocation);
+
+	glUniform1i(TexSamplerLocation, 0);
+
+	// 카메라 변환
+	Camera.Update();
+	Camera.draw_prepare(ViewPosLocation, "View_Pos");
+
+	// 투영 변환
+	glm::mat4 Projection_Mat = glm::mat4(1.0f);
+	Projection_Mat = glm::perspective(glm::radians(45.f), 1.f, 0.1f, 50.f);
+	glUniformMatrix4fv(ProjectionLocation, 1, GL_FALSE, &Projection_Mat[0][0]);
+	glUniformMatrix4fv(CameraLocation, 1, GL_FALSE, glm::value_ptr(Camera.Camera_Mat));
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	glEnable(GL_DEPTH_TEST);
+
+	// 광원
+	Light.Update();
+	Light.draw_prepare(PosLocation, "Pos");
+	Light.draw_prepare(ColorLocation, "Color");
+	Light.draw_prepare(TexorColorLocation, "Color_bool");
+	Light.draw_prepare(NormalLocation, "Normal");
+	Light.draw_prepare(WorldTransLocation, "World");
+	Light.draw_prepare(LightPosLocation, "LightPos");
+	Light.draw_prepare(LightColorLocation, "LightColor");
+	Light.draw("solid");
+
+	lineObj.Update();
+	lineObj.draw_prepare(PosLocation, "Pos");
+	lineObj.draw_prepare(ColorLocation, "Color");
+	glUniform1i(TexorColorLocation, false);
+	lineObj.draw_prepare(WorldTransLocation, "World");
+	lineObj.draw();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	Object[mod].Update();
+	Object[mod].draw_prepare(PosLocation, "Pos");
+	Object[mod].draw_prepare(WorldTransLocation, "World");
+	Object[mod].draw_prepare(NormalLocation, "Normal");
+	Object[mod].draw_prepare(UvLocation, "UV");
+	Object[mod].draw_prepare(false, "Texture");
+	Object[mod].draw_prepare(TexorColorLocation, "Texture_bool");
+	glUniform1f(DistanceLocation, distance(Light.pos, Object[mod].pos));
+	Object[mod].draw("solid");
+
+	Floor.Update();
+	Floor.draw_prepare(PosLocation, "Pos");
+	Floor.draw_prepare(WorldTransLocation, "World");
+	Floor.draw_prepare(NormalLocation, "Normal");
+	Floor.draw_prepare(UvLocation, "UV");
+	Floor.draw_prepare(false, "Texture");
+	Floor.draw_prepare(TexorColorLocation, "Texture_bool");
+	glUniform1f(DistanceLocation, distance(Light.pos, Floor.pos));
+	Floor.draw("solid");
+
+	for (int i = 0; i < Cube.size(); ++i) {
+		Cube[i].Update();
+		Cube[i].draw_prepare(PosLocation, "Pos");
+		Cube[i].draw_prepare(WorldTransLocation, "World");
+		Cube[i].draw_prepare(NormalLocation, "Normal");
+		Cube[i].draw_prepare(UvLocation, "UV");
+		Cube[i].draw_prepare(false, "Texture");
+		Cube[i].draw_prepare(TexorColorLocation, "Texture_bool");
+		glUniform1f(DistanceLocation, distance(Light.pos, Cube[i].pos));
+		Cube[i].draw("solid");
+	}
+
+
+	glDisable(GL_BLEND);
+
+
+	glDisable(GL_DEPTH_TEST);
+
+	glDisableVertexAttribArray(PosLocation);
+	glDisableVertexAttribArray(ColorLocation);
+	glDisableVertexAttribArray(NormalLocation);
+	glDisableVertexAttribArray(UvLocation);
+	glDisableVertexAttribArray(WorldTransLocation);
+	glDisableVertexAttribArray(LightPosLocation);
+	glDisableVertexAttribArray(LightColorLocation);
+	glDisableVertexAttribArray(ViewPosLocation);
+	glDisableVertexAttribArray(ProjectionLocation);
+	glDisableVertexAttribArray(CameraLocation);
+	glDisableVertexAttribArray(DistanceLocation);
+	glDisableVertexAttribArray(TexorColorLocation);
+	glDisableVertexAttribArray(TexSamplerLocation);
+
+	glutSwapBuffers();
+}
+
+void TimerFunction(int value)
+{
+	switch (value)
+	{
+	case 1: {
+
+		break;
+	}
+	default:
+		break;
+	}
+
+	glutPostRedisplay(); // 화면 재 출력
+	glutTimerFunc(10, TimerFunction, 1);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'i':
+	case 'I': {
+		Object[mod].rotate_theta.y = 180.f;
+		Object[mod].pos.z += -0.01f;
+		break;
+	}
+	case 'k':
+	case 'K': {
+		Object[mod].rotate_theta.y = 0.f;
+		Object[mod].pos.z += 0.01f;
+		break;
+	}
+	case 'j':
+	case 'J': {
+		Object[mod].rotate_theta.y = 270.f;
+		Object[mod].pos.x += -0.01f;
+		break;
+	}
+	case 'l':
+	case 'L': {
+		Object[mod].rotate_theta.y = 90.f;
+		Object[mod].pos.x += 0.01f;
+		break;
+	}
+	case 'c':
+	case 'C': {
+		if (mod) mod = 0;
+		else mod = 1;
+		break;
+	}
+	case 'a':
+	case 'A': {
+		Light.revolve_theta.y -= 5.f;
+		break;
+	}
+	case 'd':
+	case 'D': {
+		Light.revolve_theta.y += 5.f;
+		break;
+	}
+	case 'n':
+	case 'N': {
+		if (Light.pos.z > 0.5f)
+			Light.pos += glm::vec3{ 0.0f, 0.f, -0.02f };
+		break;
+	}
+	case 'f':
+	case 'F': {
+		if (Light.pos.z < 2.f)
+			Light.pos += glm::vec3{ 0.0f, 0.f, 0.02f };
+		break;
+	}
+	case 'q':
+	case 'Q': {
+		exit(829);
+	}
+	default:
+		break;
+	}
+
+	glutPostRedisplay(); // 화면 재 출력
+}
+
+void Special_Keyboard(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP: {
+		if (Light.L_color.x < 2.f)
+			Light.L_color += glm::vec3{ 0.1f, 0.1f, 0.1f };
+		break;
+	}
+	case GLUT_KEY_DOWN: {
+		if (Light.L_color.x > 0.3f)
+			Light.L_color -= glm::vec3{ 0.1f, 0.1f, 0.1f };
+		break;
+	}
+	default:
+		break;
+	}
+
+	glutPostRedisplay(); // 화면 재 출력
+}
+
+GLvoid Mouse(int button, int state, int x, int y)
+{
+	glm::vec3 m = { (x - (winSizex / 2)) / (winSizex / 2), -(y - (winSizey / 2)) / (winSizey / 2), 0.0f };
+
+	if (state == GLUT_DOWN) {
+		if (button == GLUT_LEFT_BUTTON) {
+			Lbt = true;
+			click_mouse = m;
+		}
+	}
+	else if (state == GLUT_DOWN) {
+		if (button == GLUT_LEFT_BUTTON) {
+			Lbt = false;
+			click_mouse = glm::vec3{ 0.f, 0.f, 0.f };
+		}
+	}
+}
+
+GLvoid Motion(int x, int y)
+{
+	if (Lbt) {
+		glm::vec3 m = { (x - (winSizex / 2)) / (winSizex / 2), -(y - (winSizey / 2)) / (winSizey / 2), 0.0f };
+
+		if (m.x < click_mouse.x)
+			Camera.revolve_theta.y += 1.f;
+		else if (m.x > click_mouse.x)
+			Camera.revolve_theta.y += -1.f;
+
+		if (m.y < click_mouse.y)
+			Camera.revolve_theta.x += 1.f;
+		else if (m.y > click_mouse.y)
+			Camera.revolve_theta.x += -1.f;
+
+		click_mouse = m;
+	}
+}
+
+void MouseWheel(int wheel, int diretion, int x, int y)
+{
+	// 줌인
+	if (diretion > 0) {
+		Camera.pos.z -= 0.1f;
+	}
+	// 줌아웃
+	else if (diretion < 0) {
+		Camera.pos.z += 0.1f;
+	}
+}
+
+void Init()
+{
+	glBindVertexArray(vao);
+	// 카메라
+	Camera.pos = glm::vec3{ 0.f, 0.f, 3.f };
+
+	// Light
+	{
+		std::ifstream inputFile("./OBJ/sphere.obj");
+
+		if (inputFile.is_open())
+			Light.objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Light.pos = glm::vec3{ 0.f, 0.7f, 1.f };
+		Light.scale = glm::vec3{ 0.05f, 0.05f, 0.05f };
+
+		std::vector<glm::vec3> color;
+		glm::vec3 a{ 0.23f, 0.52f, 1.0f };
+		for (int i = 0; i < Light.face_cnt * 3; ++i) {
+			color.emplace_back(a);
+		}
+		Light.L_color = glm::vec3{ 1.f, 1.f, 1.f };
+
+		glGenBuffers(1, &Light.v_color);
+		glBindBuffer(GL_ARRAY_BUFFER, Light.v_color);
+		glBufferData(GL_ARRAY_BUFFER, color.size() * sizeof(glm::vec3), color.data(), GL_STATIC_DRAW);
+	}
+	//  Object
+	{
+		{
+			std::ifstream inputFile("./OBJ/빼꼼/Player_1.obj");
+
+			if (inputFile.is_open())
+				Object[0].objLoad(inputFile);
+			else
+				std::cerr << "Failed to obj file" << std::endl;
+
+			Object[0].pos = glm::vec3{ 0.f, 0.f, 0.f };
+			Object[0].scale = glm::vec3{ 0.2f, 0.2f, 0.2f };
+			Object[0].midpos = glm::vec3{ 0.f, 0.f, 0.f };
+
+			Object[0].imgLoad("./OBJ/빼꼼/Backom_BODY_d_j.png");
+		}
+		{
+			std::ifstream inputFile("./OBJ/강쥐/body.obj");
+
+			if (inputFile.is_open())
+				Object[1].objLoad(inputFile);
+			else
+				std::cerr << "Failed to obj file" << std::endl;
+
+			Object[1].pos = glm::vec3{ 0.f, 0.f, 0.f };
+			Object[1].midpos = glm::vec3{ 0.f, 0.f, 0.f };
+			Object[1].scale = glm::vec3{ 0.2f, 0.2f, 0.2f };
+
+			Object[1].imgLoad("./OBJ/강쥐/Dog.png");
+		}
+	}
+
+	// Floor
+	{
+		std::ifstream inputFile("./OBJ/cube_tex.obj");
+
+		if (inputFile.is_open())
+			Floor.objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Floor.pos = glm::vec3{ 0.f, 0.f, 0.f };
+		Floor.scale = glm::vec3{ 3.f, 0.02f, 3.f };
+
+		Floor.imgLoad("./IMG/체크무늬.png");
+	}
+
+	// 장애물
+	{
+		std::ifstream inputFile("./OBJ/cube_tex.obj");
+		Cube.emplace_back();
+
+		if (inputFile.is_open())
+			Cube.back().objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Cube.back().scale = glm::vec3{ 0.2f, 0.2f, 0.2f };
+		Cube.back().pos = glm::vec3{ 0.5f, 0.25 * Cube.back().scale.y, 0.5f };
+
+		Cube.back().imgLoad("./IMG/반투명보라.png");
+	}
+	{
+		std::ifstream inputFile("./OBJ/cube_tex.obj");
+		Cube.emplace_back();
+
+		if (inputFile.is_open())
+			Cube.back().objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Cube.back().scale = glm::vec3{ 0.1f, 1.f, 0.1f };
+		Cube.back().pos = glm::vec3{ -0.5f, 0.25 * Cube.back().scale.y, -0.5f };
+
+		Cube.back().imgLoad("./IMG/반투명파랑.png");
+	}
+	{
+		std::ifstream inputFile("./OBJ/cube_tex.obj");
+		Cube.emplace_back();
+
+		if (inputFile.is_open())
+			Cube.back().objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Cube.back().scale = glm::vec3{ 0.1f, 1.f, 0.1f };
+		Cube.back().pos = glm::vec3{ -0.3f, 0.25 * Cube.back().scale.y, -0.5f };
+
+		Cube.back().imgLoad("./IMG/반투명파랑.png");
+	}
+	{
+		std::ifstream inputFile("./OBJ/cube_tex.obj");
+		Cube.emplace_back();
+
+		if (inputFile.is_open())
+			Cube.back().objLoad(inputFile);
+		else
+			std::cerr << "Failed to obj file" << std::endl;
+
+		Cube.back().scale = glm::vec3{ 0.4f, 0.1f, 0.1f };
+		Cube.back().pos = glm::vec3{ -0.4f, 0.5f, -0.5f };
+
+		Cube.back().imgLoad("./IMG/반투명파랑.png");
+	}
+
+	// X축 Y축
+	{
+		glm::vec3 line[6]{
+			{1.0f, 0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f},
+			{0.0f, 1.0f, 0.0f}, {0.0f, -1.0f, 0.0f},
+			{0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}
+		};
+
+		glGenBuffers(1, &V_pos_Line);
+		glBindBuffer(GL_ARRAY_BUFFER, V_pos_Line);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(line), line, GL_STATIC_DRAW);
+
+		lineObj = GLLine({ 0.0f, 0.0f, 0.0f });
+	}
+
+
+
+}
+
+//--- 다시그리기 콜백 함수
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void InitBuffer()
+{
+	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
+	glBindVertexArray(vao); //--- VAO를 바인드하기
+}
+
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("3d_vertex_v5_light.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cout << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("uv_fragment.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cout << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading 
+	if (!fptr) // Return NULL on failure 
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file 
+	length = ftell(fptr); // Find out how many bytes into the file we are 
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator 
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file 
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer 
+	fclose(fptr); // Close the file 
+	buf[length] = 0; // Null terminator 
+	return buf; // Return the buffer 
+}
